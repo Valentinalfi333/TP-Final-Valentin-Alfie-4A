bcc30a06ac4eda1bc5c6cccf1d249f7de9c041c3
valen <48178972@est.ort.edu.ar>
2024-06-19 09:59:52 -0300

    TP Final Valentin Alfie 4°A

---
+++ Assets/Codigo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Codigo : MonoBehaviour
+{
+    public GameObject[] Objetos;
+    public int elemento;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i < Objetos.Length; i++)
+        {
+            Objetos[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+            elemento++;
+            if (27 <= elemento)
+                {
+                    elemento = 26;
+                }
+            Objetos[elemento].SetActive(true);
+            Objetos[elemento-1].SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            elemento--;
+            if (elemento < 0)
+            {
+                elemento = 0;
+            }
+            Objetos[elemento].SetActive(true);
+            Objetos[elemento + 1].SetActive(false);
+        } 
+    }
+
+
+}
